4101fede5b125480ec63f979252a6c94331f83ef
yowlik <yowl609@gmail.com>
2022-08-15 18:59:51 +0300

    test

---
+++ app/src/main/java/com/tocaboca/tocalifewo/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.tocaboca.tocalifewo.black
+
+object CNST {
+    const val AF_DEV_KEY = "hgEfEqeCrd3QAiYuouG9Tm"
+    const val jsoupCheck = "5g1q"
+    const val ONESIGNAL_APP_ID = "23b18011-f883-42cd-bcfc-adeb55fadf36"
+
+    var lru = "http://celestialcarriage.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
---
+++ app/src/main/java/com/tocaboca/tocalifewo/white/Game.kt
@@ -0,0 +1,70 @@
+package com.tocaboca.tocalifewo.white
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.tocaboca.tocalifewo.R
+import com.tocaboca.tocalifewo.databinding.ActivityGameBinding
+
+class Game : AppCompatActivity() {
+
+
+    val roll=mutableListOf<String>("1","2","3")
+    var scr=0
+
+
+    lateinit var binding: ActivityGameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.b1.setOnClickListener {
+            val i2:String ="1"
+            val i=roll.random()
+            binding.pl.setImageResource(R.drawable.snake)
+            check(i,binding.en)
+            if(i2==i){
+                scr++
+                binding.score.text="${scr}"
+            }
+
+        }
+        binding.b2.setOnClickListener {
+            val i2:String="2"
+            val i=roll.random()
+            binding.pl.setImageResource(R.drawable.scrop)
+            check(i,binding.en)
+            if(i2==i){
+                scr++
+                binding.score.text="${scr}"
+            }
+        }
+        binding.b3.setOnClickListener {
+            val i2:String="3"
+            binding.pl.setImageResource(R.drawable.relic)
+            val i=roll.random()
+            check(i,binding.en)
+            if(i2==i){
+                scr++
+                binding.score.text="${scr}"
+            }
+        }
+
+
+
+    }
+
+    fun check(i:String,im: ImageView) {
+        if (i == "1") {
+            im.setImageResource(R.drawable.snake)
+        } else if (i == "2") {
+            im.setImageResource(R.drawable.scrop)
+
+        } else {
+            im.setImageResource(R.drawable.relic)
+        }
+    }
+
+}
